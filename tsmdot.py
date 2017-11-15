e278123a10ec6b11327b0f4d83404d291f7c96e7
richgreen1976 <richgreen1976@users.noreply.github.com>
2017-11-15 20:59:59 +0000

    Update tsmdot.py

--- tsmdot.py
+++ tsmdot.py
@@ -7,62 +7,34 @@ from microdotphat import clear, show, write_string, scroll, set_decimal
 READ_API_KEY='HSYK0UT7YHMUPP35'
 CHANNEL_ID='256881'
 
-def main():
+def getdata():
     conn = urllib2.urlopen("https://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
                            % (CHANNEL_ID,READ_API_KEY))
     response = conn.read()
     print "http status code=%s" % (conn.getcode())
-
-    while  conn.getcode() != 200:
-        conn = urllib2.urlopen("https://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
-                               % (CHANNEL_ID,READ_API_KEY))
-        response = conn.read()
-        sleep.time(1)
-        set_decimal(0, 1)
-        set_decimal(1, 1)
-        set_decimal(2, 1)
-        set_decimal(3, 1)
-        set_decimal(4, 1)
-        set_decimal(5, 1)
-        show()
-        sleep.time(1)
-        set_decimal(0, 0)
-        set_decimal(1, 0)
-        set_decimal(2, 0)
-        set_decimal(3, 0)
-        set_decimal(4, 0)
-        set_decimal(5, 0)
-        show()
-        print "http status code=%s" % (conn.getcode())
-        # conn.close()
-        
-    set_decimal(0, 0)
-    set_decimal(1, 0)
-    set_decimal(2, 0)
-    set_decimal(3, 0)
-    set_decimal(4, 0)
-    set_decimal(5, 0)
-    show()
+    responsecode = conn.getcode()
     
-    data=json.loads(response)
-    print data['field1'],data['field2'],data['field3'],data['created_at']
-    outtemp = data['field1']
-    temp = float(outtemp)
-    tempstring = str(round(temp,1))
-
-    print("""Displays the current outdoor temperature.
-    Press Ctrl+C to exit.
-    """)
-    conn.close()
-
+    while responsecode = 200:
+        data=json.loads(response)
+        print data['field1'],data['field2'],data['field3'],data['created_at']
+        outtemp = data['field1']
+        temp = float(outtemp)
+        tempstring = str(round(temp,1))
+        print("""Displays the current outdoor temperature.
+        Press Ctrl+C to exit.
+        """)
+        conn.close()
+    else
+        
+        
+def display():
     write_string(" " + tempstring + "C", kerning=False)
-
     set_decimal(0, 1)
     set_decimal(1, 1)
     show()
     time.sleep(1)
     clear()
-x = 0
+    x = 0
 
     while x < 30:
         write_string(" " + tempstring + "C", kerning=False)
@@ -79,7 +51,13 @@ x = 0
 
 while True:
         try:
-           main()
+           getdata()
+           
+           while responsecode != 200:
+                getdata()
+           else
+                display()
+                
         except KeyboardInterrupt:
                 clear()
                 sys.exit(-1)
